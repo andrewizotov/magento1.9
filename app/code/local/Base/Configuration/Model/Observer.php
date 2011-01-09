e31238b59652ac9927d42107de174e5f0491dc75
andrew <andrew.izotov@yahoo.com>
2011-01-09 01:13:57 +0100

    add observer

---
+++ app/code/local/Base/Configuration/Model/Observer.php
@@ -0,0 +1,12 @@
+<?php
+
+class Base_Configuration_Model_Observer
+{
+    public function test()
+    {
+        /* @var $cookie Mage_Core_Model_Cookie */
+        $cookie = Mage::getModel('core/cookie');
+        $cookie->set('test2','test');
+        Mage::log('test2',null,'base.log');
+    }
+}
